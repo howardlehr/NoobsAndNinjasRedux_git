b32bb257ca3758d4393c910c192a8ddc54a16579
HOWARDDESKTOP\Howard Lehr <howard.lehr@gmail.com>
2018-09-07 09:59:02 -0700

    Made Noob move towards ship

---
+++ Assets/Scripts/NoobControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoobControl : MonoBehaviour {
+
+    public Transform player;
+    public float noobSpeed = .025f;
+    
+	void Start () {
+		
+	}
+	
+	void Update ()
+    {
+		if (Vector2.Distance(transform.position,player.transform.position) < 50)
+        {
+            if (player.transform.position.y < -4.42)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.transform.position.x,transform.position.y),noobSpeed);
+            }
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
